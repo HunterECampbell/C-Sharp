86b299b9dfd51cca1c149814e4e46d3178f3034e
Hunter Campbell <hcnureth@gmail.com>
2019-04-19 19:40:03 -0600

    Upload Palindrome

---
+++ Palindrome.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+
+        static bool Check_Palindrome(string word)
+        {
+            char[] word_Char = word.ToCharArray();
+            Array.Reverse(word_Char);
+
+            string reverse_Word = new string(word_Char);
+
+
+            if (word.ToUpper() == reverse_Word.ToUpper())
+            {
+                Console.WriteLine("Your word is: " + word + ".");
+                Console.WriteLine("Your word reversed is: " + reverse_Word + ".");
+                Console.WriteLine(word + " is a palindrome!");
+                Console.WriteLine();
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Your word is: " + word + ".");
+                Console.WriteLine("Your word reversed is: " + reverse_Word + ".");
+                Console.WriteLine(word + " is not a palindrome.");
+                Console.WriteLine();
+                return false;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Check_Palindrome("Hello");
+            Check_Palindrome("Racecar");
+            Check_Palindrome("Rollercoaster");
+            Check_Palindrome("Xanax");
+        }
+    }
+}
